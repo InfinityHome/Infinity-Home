cca6109c8df3c0d248ec5f9a3b74aae9c3660bca
Mann Patel <you@example.com>
2021-10-01 21:18:32 -0400

    added dependencies

--- App.tsx
+++ App.tsx
@@ -1,35 +1,34 @@
-import React, { useEffect, useState } from "react";
-import * as Font from "expo-font";
-import { ActivityIndicator, View } from "react-native";
-import Navigation from "./components/Navigation";
+import React, { useEffect, useState } from 'react';
+import * as Font from 'expo-font';
+import { ActivityIndicator, View } from 'react-native';
+import Navigation from './components/Navigation';
 
 const App: React.FC = () => {
-	const [fontLoaded, setFontLoaded] = useState<boolean>(false);
+  const [fontLoaded, setFontLoaded] = useState<boolean>(false);
 
-	useEffect(() => {
-		const fetchFont = async () =>
-			await Font.loadAsync({
-				"Radley-Italic": require("./assets/fonts/Radley-Italic.ttf"),
-				"Radley-Regular": require("./assets/fonts/Radley-Regular.ttf"),
-				"Quintessential-Regular": require("./assets/fonts/Quintessential-Regular.ttf")
-			});
+  useEffect(() => {
+    const fetchFont = async () => await Font.loadAsync({
+      'Radley-Italic': require('./assets/fonts/Radley-Italic.ttf'),
+      'Radley-Regular': require('./assets/fonts/Radley-Regular.ttf'),
+      'Quintessential-Regular': require('./assets/fonts/Quintessential-Regular.ttf'),
+    });
 
-		fetchFont().then(() => {
-			setFontLoaded(true);
-		});
-	}, []);
+    fetchFont().then(() => {
+      setFontLoaded(true);
+    });
+  }, []);
 
-	return (
-		<>
-			{fontLoaded ? (
-				<Navigation />
-			) : (
-				<View style={{ flex: 1, justifyContent: "center" }}>
-					<ActivityIndicator size="large" color="gray" />
-				</View>
-			)}
-		</>
-	);
+  return (
+    <>
+      {fontLoaded ? (
+        <Navigation />
+      ) : (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator size="large" color="gray" />
+        </View>
+      )}
+    </>
+  );
 };
 
 export default App;
