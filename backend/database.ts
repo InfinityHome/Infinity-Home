4f883884dc51310257db40d85d5b7d656e4a2bcc
Parth Patel <ppatil91099@gmail.com>
2021-10-03 21:16:28 -0400

    added file

---
+++ backend/database.ts
@@ -0,0 +1,13 @@
+import firebase from "../src/constants/firebase";
+
+class DataBase {
+
+
+    constructor() {
+        const database = firebase.database();
+        
+    }
+
+}
+
+export const database = new DataBase();
