d969ebdfa7f178e55a4e3e5f561dd317aa93a38e
Parth <ppatil91099@gmail.com>
2021-10-05 11:01:39 -0400

    added db file

---
+++ src/firebase/firebaseDB.ts
@@ -0,0 +1,21 @@
+import firebase from "./config";
+
+class DataBase {
+    database: firebase.database.Database;
+
+    constructor() {
+        this.database = firebase.database();
+    }
+
+    readDatabase(table: string) {
+        const tableRef = this.database.ref(table);
+        tableRef.on('value', (snapshot) => {
+            console.log(snapshot.val());
+        }, (errorObject: { name: string; }) => {
+            console.log('The read failed: ' + errorObject.name);
+        });
+    }
+
+}
+
+export const database = new DataBase(); 
