2e481e2ab096b1e4183f9ecb42ee6900969a90ce
Akash Patel <ap.akash.patel1999+github@gmail.com>
2021-10-06 18:51:19 -0400

    did the bottom nav bar

--- App.tsx
+++ App.tsx
@@ -1,35 +1,33 @@
-import React, { useEffect, useState } from "react";
-import * as Font from "expo-font";
-import { ActivityIndicator, View } from "react-native";
-import Navigation from "./src/components/Navigation";
-import Orders from "./src/screens/Orders";
+import React, { useEffect, useState } from 'react';
+import * as Font from 'expo-font';
+import { ActivityIndicator, View } from 'react-native';
+import Navigation from './src/components/Navigation';
 
 const App: React.FC = () => {
-	const [fontLoaded, setFontLoaded] = useState<boolean>(false);
-	useEffect(() => {
-		const fetchFont = async () =>
-			await Font.loadAsync({
-				"Radley-Italic": require("./assets/fonts/Radley-Italic.ttf"),
-				"Radley-Regular": require("./assets/fonts/Radley-Regular.ttf"),
-				"Quintessential-Regular": require("./assets/fonts/Quintessential-Regular.ttf")
-			});
+  const [fontLoaded, setFontLoaded] = useState<boolean>(false);
+  useEffect(() => {
+    const fetchFont = async () =>
+      await Font.loadAsync({
+        'Radley-Italic': require('./assets/fonts/Radley-Italic.ttf'),
+        'Radley-Regular': require('./assets/fonts/Radley-Regular.ttf'),
+        'Quintessential-Regular': require('./assets/fonts/Quintessential-Regular.ttf'),
+      });
+    fetchFont().then(() => {
+      setFontLoaded(true);
+    });
+  }, []);
 
-		fetchFont().then(() => {
-			setFontLoaded(true);
-		});
-	}, []);
-
-	return (
-		<>
-			{fontLoaded ? (
-				<Navigation />
-			) : (
-				<View style={{ flex: 1, justifyContent: "center" }}>
-					<ActivityIndicator size="large" color="gray" />
-				</View>
-			)}
-		</>
-	);
+  return (
+    <>
+      {fontLoaded ? (
+        <Navigation />
+      ) : (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator size="large" color="gray" />
+        </View>
+      )}
+    </>
+  );
 };
 
 export default App;
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,25 +1,58 @@
 import React from 'react';
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import SignUp from '../screens/SignUp';
 import Login from '../screens/Login';
 import SignIn from '../screens/SignIn';
 import Home from '../screens/Home';
-export type loginStackParams = {SignUp : undefined, Login : undefined, SignIn : undefined, Home : undefined}
+import Orders from '../screens/Orders';
+import Account from '../screens/Account';
 
-const Stack = createNativeStackNavigator<loginStackParams>();
+export type StackParams = {
+  SignUp: undefined;
+  Login: undefined;
+  SignIn: undefined;
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator();
+const LoginNavigation: React.FC = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="SignIn" component={SignIn} />
+    </Stack.Navigator>
+  );
+};
+
+const Tab = createBottomTabNavigator();
+const BottomNavigation: React.FC = () => {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Orders" component={Orders} />
+      <Tab.Screen name="Account" component={Account} />
+    </Tab.Navigator>
+  );
+};
 
 const Navigation: React.FC = () => {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="SignUp" component={SignUp} />
-          <Stack.Screen name="SignIn" component={SignIn} />
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen
+          name="Home"
+          component={BottomNavigation}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default Navigation;
