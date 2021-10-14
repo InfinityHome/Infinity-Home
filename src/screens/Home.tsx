cdbe6a0d0c36c999d70b19d572d681a8c154964d
Parth <ppatil91099@gmail.com>
2021-10-13 20:33:46 -0400

    Revert "implemented services using DB"
    This reverts commit ce3067a148b1d8a984e554dd56c418033dd88ffd.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,19 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView, StatusBar, ScrollView, Button } from 'react-native';
 import { authMethod } from '../firebase/config';
 import Header from '../components/Header';
 import Services from '../components/Services';
 import Search from '../components/Search';
-import { database } from '../firebase/firebaseDB';
 
 export type ServiceListType = {
-  serviceDetails: {
-    company: Record<string, { resourcesAllocated: number }>;
-    serviceIcon: string;
-    serviceName: string;
-  };
-  serviceID: string;
+  Service: string;
+  ServiceIcon: string;
+  IconColor: string;
 }[];
+
+const ServiceList: ServiceListType = [
+  {
+    Service: 'Plumbing',
+    ServiceIcon: 'plumbing',
+    IconColor: 'gray',
+  },
+  {
+    Service: 'Electrical',
+    ServiceIcon: 'electrical-services',
+    IconColor: 'blue',
+  },
+  {
+    Service: 'Lawn',
+    ServiceIcon: 'grass',
+    IconColor: 'green',
+  },
+
+  {
+    Service: 'Painting',
+    ServiceIcon: 'format-paint',
+    IconColor: 'red',
+  },
+  {
+    Service: 'Hvac',
+    ServiceIcon: 'hvac',
+    IconColor: 'gray',
+  },
+  {
+    Service: 'Roofing',
+    ServiceIcon: 'roofing',
+    IconColor: 'brown',
+  },
+  { Service: 'Gutter', ServiceIcon: 'filter-alt', IconColor: 'black' },
+];
 
 const Home: React.FC = () => {
   const handleSignOut = async () => {
@@ -23,17 +54,8 @@ const Home: React.FC = () => {
       console.log(error);
     }
   };
-  const [serviceList, setServiceList] = useState([]);
-  const [finalFilteredList, setFinalFilteredList] = useState<ServiceListType>(
-    []
-  );
-
-  useEffect(() => {
-    database.readServices().then((data) => {
-      setServiceList(data);
-      setFinalFilteredList(data);
-    });
-  }, []);
+  const [finalFilteredList, setFinalFilteredList] =
+    useState<ServiceListType>(ServiceList);
 
   return (
     <SafeAreaView
@@ -44,7 +66,7 @@ const Home: React.FC = () => {
       <Header name="Infinity Home" font="Italic" size={42} />
       <Search
         setFinalFilteredList={setFinalFilteredList}
-        serviceList={serviceList}
+        ServiceList={ServiceList}
       />
       <Button title="Log Out" onPress={handleSignOut} />
       <ScrollView showsVerticalScrollIndicator={false}>
