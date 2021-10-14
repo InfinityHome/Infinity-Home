2e61d8e9b9848a9464395d8e06d85c75856498bc
Akash Patel <ap.akash.patel1999+github@gmail.com>
2021-10-13 21:02:59 -0400

    convert reg form to formik and yup (#78)
    * convert reg form to formik and yup
    
    * took out a case for error
    
    * removed css
    
    * took out .min for confirmpassword

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,71 +1,122 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, TextInput, Alert } from 'react-native';
+import React from 'react';
+import { StyleSheet, Alert, SafeAreaView } from 'react-native';
 import { authMethod } from '../firebase/config';
 import Text from '../customs/CustomText';
 import Button from '../customs/CustomButton';
+import TextField from '../components/TextField';
+import { LoginNavProps } from '../Navigation/Params';
+import { Formik, FormikProps } from 'formik';
+import * as Yup from 'yup';
+import Validator from 'email-validator';
 
-const SignIn: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const SignIn: React.FC<LoginNavProps<'SignIn'>> = ({ navigation }) => {
+  const SignInSchema = Yup.object().shape({
+    email: Yup.string().email().required('An email is required'),
+    password: Yup.string()
+      .required()
+      .min(6, 'Your password has to have at least 6 characters'),
+  });
 
-  const onLogin = async () => {
-    try {
-      if (email !== '' && password !== '') {
-        await authMethod.signInWithEmailAndPassword(email, password);
-      }
-    } catch ({ message }) {
-      Alert.alert(
-        'Sign In Failed',
-        JSON.stringify(message, Object.getOwnPropertyNames(message)),
-        [
-          {
-            text: 'Try Again',
-          },
-        ]
-      );
-    }
+  const onLogin = async (email: string, password: string) => {
+    await authMethod
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        switch (error.code) {
+          case 'auth/wrong-password':
+            Alert.alert('Oops', 'Wrong Password', [{ text: 'Try Again' }]);
+            break;
+          case 'auth/user-not-found':
+            Alert.alert('Sorry', 'User does not exist', [
+              { text: 'Cancel' },
+              {
+                text: 'Sign Up',
+                onPress: () => navigation.replace('SignUp'),
+              },
+            ]);
+            break;
+          default:
+            Alert.alert('Error', 'Something Went Wrong', [
+              { text: 'Try Again' },
+            ]);
+            break;
+        }
+      });
   };
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: '#9BBCFD',
+        paddingHorizontal: 20,
+        justifyContent: 'center',
+      }}>
       <Text style={styles.text}>Welcome Back</Text>
       <Text style={styles.text1}>Sign In</Text>
-      <TextInput
-        style={styles.input}
-        placeholder={'Email'}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder={'Password'}
-        secureTextEntry
-        onChangeText={(text) => setPassword(text)}
-      />
-      <Button title="Sign In" onPress={onLogin} />
-    </View>
+
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        onSubmit={(values) => {
+          onLogin(values.email, values.password);
+        }}
+        validationSchema={SignInSchema}
+        validateOnMount>
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          isValid,
+        }: FormikProps<{ email: string; password: string }>) => (
+          <>
+            <TextField
+              placeholder="Email"
+              name="email"
+              handleChange={handleChange}
+              handleBlur={handleBlur}
+              keyboardType="email-address"
+              value={values.email}
+              vadilate={
+                values.email.length < 1 || Validator.validate(values.email)
+                  ? '#000'
+                  : 'red'
+              }
+            />
+            <TextField
+              placeholder="Password"
+              name="password"
+              handleBlur={handleBlur}
+              handleChange={handleChange}
+              secureTextEntry
+              value={values.password}
+              vadilate={
+                1 > values.password.length || values.password.length >= 6
+                  ? '#000'
+                  : 'red'
+              }
+            />
+            <Button
+              title="Sign In"
+              buttonOpacity={{ opacity: isValid ? 1 : 0.5 }}
+              onPress={handleSubmit}
+            />
+          </>
+        )}
+      </Formik>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+    textAlign: 'center',
+    marginLeft: -50,
   },
   text1: {
     fontSize: 30,
+    textAlign: 'center',
     paddingLeft: 170,
-  },
-  container: {
-    flex: 1,
-    paddingTop: 90,
-    padding: 20,
-    backgroundColor: '#9BBCFD',
-  },
-  input: {
-    backgroundColor: 'white',
-    width: '100%',
-    marginVertical: 10,
-    padding: 20,
-    borderRadius: 10,
   },
 });
 
