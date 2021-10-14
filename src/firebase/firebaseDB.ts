99ca0c965d478f7549db5fa1d733647f139b1498
Parth <ppatil91099@gmail.com>
2021-10-13 20:30:44 -0400

    res comments

--- src/firebase/firebaseDB.ts
+++ src/firebase/firebaseDB.ts
@@ -2,7 +2,7 @@ import {firebase} from "./config";
 
 class DataBase {
     database: firebase.database.Database;
-    serviceTable: any;
+    serviceTable: { serviceId: string | null; serviceDetails: any; }[];
 
     constructor() {
         this.database = firebase.database();
@@ -13,13 +13,13 @@ class DataBase {
      * @return Promise<any returns the fetched data from the services object in firebase 
      * reference this function as such: database.readServices().then((data) => {setServices(data);})
      */
-    async readServices(): Promise<any> {
+    async readServices(): Promise<{serviceId: string|null; serviceDetails: any}[]> {
         const serviceRef = this.database.ref('/services')
         await serviceRef.once('value', (snapshot) => {
             snapshot.forEach((child) => {
-                const serviceID = child.key;
+                const serviceId = child.key;
                 const serviceDetails = child.val()
-                this.serviceTable.push({ serviceID, serviceDetails })
+                this.serviceTable.push({ serviceId, serviceDetails })
             })
         }, (errorObject: { name: string; }) => {
             console.log('The read failed: ' + errorObject.name);
