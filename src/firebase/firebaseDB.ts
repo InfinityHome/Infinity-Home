97c0f209bc08d34ab57436317492d48fee1d7989
Parth Patel <ppatil91099@gmail.com>
2021-10-13 21:11:47 -0400

    final touches

--- src/firebase/firebaseDB.ts
+++ src/firebase/firebaseDB.ts
@@ -1,17 +1,15 @@
 import {firebase} from "./config";
 
-type ServiceTable = {
-    serviceDetails: {
-        company: Record<string, { resourcesAllocated: number }>;
-        serviceIcon: string;
-        serviceName: string;
-      };
-      serviceId: string | null;
+type ServiceListType = {
+    companyDetails: Record<string, { resourcesAllocated: number }>;
+    serviceId: string | null;
+    serviceName: string | null;
+    serviceIcon: string | null;
 }[];
 
 class DataBase {
     database: firebase.database.Database;
-    serviceTable: ServiceTable;
+    serviceTable: ServiceListType;
     
     constructor() {
         this.database = firebase.database();
@@ -22,13 +20,15 @@ class DataBase {
      * @return Promise<any returns the fetched data from the services object in firebase 
      * reference this function as such: database.readServices().then((data) => {setServices(data);})
      */
-    async readServices(): Promise<ServiceTable> {
+    async readServices(): Promise<ServiceListType> {
         const serviceRef = this.database.ref('/services')
         await serviceRef.once('value', (snapshot) => {
             snapshot.forEach((child) => {
                 const serviceId = child.key;
-                const serviceDetails = child.val()
-                this.serviceTable.push({ serviceId, serviceDetails })
+                const serviceName = child.val().serviceName;
+                const serviceIcon = child.val().serviceIcon;
+                const companyDetails = child.val().company;
+                this.serviceTable.push({ serviceId, serviceIcon, serviceName, companyDetails })
             })
         }, (errorObject: { name: string; }) => {
             console.log('The read failed: ' + errorObject.name);
@@ -38,4 +38,4 @@ class DataBase {
 }
 
 export const database = new DataBase();
-export { ServiceTable };
+export { ServiceListType };
