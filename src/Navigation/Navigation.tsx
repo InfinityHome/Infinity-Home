4e4f65d27ebfcbad6a7764f72af3b0ad33c9d982
Akash Patel <ap.akash.patel1999+github@gmail.com>
2021-11-18 21:55:30 -0500

    added the questions for services (#99)
    * added the questions for services
    
    * removed i and npm packages and unused step progress
    
    * added other option and text box for user to type in
    
    * required the answer of question from user
    
    * added the bg color
    
    * can be more descriptive in one screen, convert {} -> [{},{}...]
    
    * made it look pretty kinda

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,31 +1,35 @@
-import React, { useEffect, useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { LoginParamList, BottomNavParamList, AccountParamList } from "./Params";
-import { authMethod, firebase } from "../firebase/config";
-import { Platform } from "react-native";
-import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
-import { Icon } from "react-native-elements";
-import SignUp from "../screens/SignUp";
-import Login from "../screens/Login";
-import SignIn from "../screens/SignIn";
-import Home from "../screens/Home";
-import Orders from "../screens/Orders";
-import Account from "../screens/Account";
-import AccountDetails from "../screens/AccountDetails";
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  LoginParamList,
+  BottomNavParamList,
+  AccountParamList,
+  HomeParamList,
+} from './Params';
+import { authMethod, firebase } from '../firebase/config';
+import { Icon } from 'react-native-elements';
+import SignUp from '../screens/SignUp';
+import Login from '../screens/Login';
+import SignIn from '../screens/SignIn';
+import Home from '../screens/Home';
+import Orders from '../screens/Orders';
+import Account from '../screens/Account';
+import AccountDetails from '../screens/AccountDetails';
+import Questions from '../screens/Questions';
+import { Platform } from 'react-native';
 
 const Stack = createNativeStackNavigator<LoginParamList>();
 const LoginNavigation: React.FC = () => {
   return (
     <Stack.Navigator
       screenOptions={{
-        title: "",
-        headerStyle: { backgroundColor: "#444956" },
+        title: '',
+        headerStyle: { backgroundColor: '#444956' },
         headerShadowVisible: false,
-        headerTintColor: "white",
-      }}
-    >
+        headerTintColor: 'white',
+      }}>
       <Stack.Screen
         name="Login"
         component={Login}
@@ -50,13 +54,40 @@ const AccountNavigation: React.FC = () => {
         name="AccountDetails"
         component={AccountDetails}
         options={{
-          title: "",
-          headerStyle: { backgroundColor: "#444956" },
+          title: '',
+          headerStyle: { backgroundColor: '#444956' },
           headerShadowVisible: false,
-          headerTintColor: "white",
+          headerTintColor: 'white',
         }}
       />
     </AccStack.Navigator>
+  );
+};
+
+const HomeStack = createNativeStackNavigator<HomeParamList>();
+const HomeNavigation: React.FC = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="HomeScreen"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <HomeStack.Screen
+        name="Questions"
+        component={Questions}
+        options={({
+          route: {
+            params: { title },
+          },
+        }) => ({
+          title: title,
+          headerStyle: { backgroundColor: '#444956' },
+          headerShadowVisible: false,
+          headerTintColor: 'white',
+        })}
+      />
+    </HomeStack.Navigator>
   );
 };
 
@@ -66,25 +97,24 @@ const BottomNavigation: React.FC = () => {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: "white",
+        tabBarActiveTintColor: 'white',
         tabBarLabelStyle: {
           fontSize: 12,
-          bottom: Platform.OS === "ios" ? 0 : 8,
+          bottom: Platform.OS === 'ios' ? 0 : 8,
         },
         tabBarHideOnKeyboard: true,
         tabBarStyle: {
-          backgroundColor: "#21242c",
+          backgroundColor: '#21242c',
           borderTopWidth: 0,
           minHeight: 55,
-          paddingTop: Platform.OS === "ios" ? 10 : 0,
+          paddingTop: Platform.OS === 'ios' ? 10 : 0,
         },
-      }}
-    >
+      }}>
       <Tab.Screen
         name="Home"
-        component={Home}
+        component={HomeNavigation}
         options={{
-          tabBarLabel: "Home",
+          tabBarLabel: 'Home',
           tabBarIcon: () => <Icon name="house" color="white" size={25} />,
         }}
       />
@@ -92,7 +122,7 @@ const BottomNavigation: React.FC = () => {
         name="Orders"
         component={Orders}
         options={{
-          tabBarLabel: "Orders",
+          tabBarLabel: 'Orders',
           tabBarIcon: () => (
             <Icon name="shopping-cart" color="white" size={25} />
           ),
@@ -102,7 +132,7 @@ const BottomNavigation: React.FC = () => {
         name="Account"
         component={AccountNavigation}
         options={{
-          tabBarLabel: "Account",
+          tabBarLabel: 'Account',
           tabBarIcon: () => <Icon name="person" color="white" size={25} />,
         }}
       />
--- src/Navigation/Params.tsx
+++ src/Navigation/Params.tsx
@@ -11,12 +11,21 @@ export type AccountParamList = {
   AccountDetails: undefined;
 };
 
+export type HomeParamList = {
+  HomeScreen: undefined;
+  Questions: { title: string };
+};
+
 export type LoginNavProps<T extends keyof LoginParamList> = {
   navigation: NativeStackNavigationProp<LoginParamList, T>;
 };
 
 export type AccountNavProps<T extends keyof AccountParamList> = {
   navigation: NativeStackNavigationProp<AccountParamList, T>;
+};
+
+export type HomeNavProps<T extends keyof HomeParamList> = {
+  navigation: NativeStackNavigationProp<HomeParamList, T>;
 };
 
 export type BottomNavParamList = {
