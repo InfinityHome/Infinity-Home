b036d879b018a97bb9ab613436910ae7cbc7e7f7
Akash Patel <ap.akash.patel1999+github@gmail.com>
2021-11-26 13:38:38 -0500

    added dotenv

--- stripe/index.js
+++ stripe/index.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import Stripe from 'stripe';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
 const port = 3000; //add your port here
-const SECRET_KEY =
-  'sk_test_51Jz4ZuFq2wD94Na9IsZ9M6Zm1JeCH3gBk9a2nSO4RrJi9O0hT70mP9wdJQdbyBGjQq4B7dRiEtJPa2JI2fBuFx7D00HI73SumM';
-
 
 //Confirm the API version from your stripe dashboard
-const stripe = new Stripe(SECRET_KEY, { apiVersion: '2020-08-27' });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2020-08-27' });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
